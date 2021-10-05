67bc8ec7da0b0c5b5271d536b620bbdf0e6f936a
Gudan3792 <k_akao@kakao.com>
2021-10-05 18:51:09 +0900

    Commit

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ app.get('/', function(req,res) {
     res.sendFile(__dirname + "/public/main.html")
 })
 
-// localhost:8484/main 브라우저에 res.sendFile() 내부의 파일이 띄워진다.
 app.get('/main', function(req,res) {
     res.sendFile(__dirname + "/public/main.html")
 })
