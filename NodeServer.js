222a32bc943b0ae567d3a1bbad0414844f611e36
Gudan3792 <k_akao@kakao.com>
2021-10-24 16:08:50 +0900

    html 1차 완성본

--- NodeServer.js
+++ NodeServer.js
@@ -1,14 +1,14 @@
-var express = require('express')
-var Server = express()
+const express = require('express');
+const app = express();
 
-Server.listen(8484);
+app.use(express.static('images'));
+app.use(express.static(__dirname + '/css'));
+app.get('/',(req,res) => res.sendFile(__dirname + '/css/main/main.html'));
 
-Server.get('/', function(req,res) {
-    res.sendFile(__dirname + "/public/main.html")
-})
+app.get('/about',(req,res)=>{
+    res.send('about page');
+});
 
-Server.get('/main', function(req,res) {
-    res.sendFile(__dirname + "/public/main.html")
-})
-
-Server.use(express.static('public'))
+app.listen(9000,function(){
+    console.log('start 9000');
+});
