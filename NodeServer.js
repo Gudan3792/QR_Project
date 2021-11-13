6e353e44a1b32832ad07171b840d618923461609
Gudan3792 <k_akao@kakao.com>
2021-11-13 22:59:40 +0900

    admin_main.ejs 에서 DB자료 수정가능하도록 코딩

--- NodeServer.js
+++ NodeServer.js
@@ -3,15 +3,18 @@ const adminModule = require("./admin_module/lib");
 const app = express();
 var db_config = require(__dirname + '/SQLjs.js');
 var conn = db_config.init();
+var bodyParser = require('body-parser');
 
 db_config.connect(conn);
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
 app.use(express.static('images'));
 app.use(express.static(__dirname + '/homepage_file'));
-// app.get('/',(req,res) => res.sendFile(__dirname + '/homepage_file/main/main.html'));
 
 app.get('/', function (req, res) {
     var sql = 'SELECT * FROM menu';    
@@ -21,8 +24,67 @@ app.get('/', function (req, res) {
     });
 });
 
-// adminModule.initMethod.initDB("127.0.0.1",3306,"root","111111","project_sql");//db 초기화 함수
-adminModule.initMethod.initAdminStaticPath("/admin_login/admin_main.html");//로그인 완료후 리다이렉트될 url
+app.get('/admin_login/admin_main', function (req, res) {
+    var sql = 'SELECT * FROM menu';    
+    conn.query(sql, function (err, rows, fields) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        else res.render('admin_main.ejs', {list : rows});
+    });
+});
+
+app.post('/writeAf', function (req, res) {
+    var body = req.body;
+    console.log(body);
+
+    var sql = 'INSERT INTO `menu` VALUES(?,?, ?, NOW())';
+    var params = [body.id, body.name, body.price];
+    console.log(sql);
+    conn.query(sql, params, function(err) {
+        if(err) console.log('query is not excuted. insert fail...\n' + err);
+        else res.redirect('/admin_login/admin_main');
+    });
+});
+
+app.post('/deleteAf', function (req, res) {
+    var body = req.body;
+    console.log(body);
+
+    var sql = 'DELETE FROM `menu` WHERE name = ?';
+    var params = [body.name];
+    console.log(sql);
+    conn.query(sql, params, function(err) {
+        if(err) console.log('query is not excuted. insert fail...\n' + err);
+        else res.redirect('/admin_login/admin_main');
+    });
+});
+
+app.post('/updataAf', function (req, res) {
+    var body = req.body;
+    console.log(body);
+
+    var sql = 'UPDATE `menu` SET name = ? WHERE id =?';
+    var params = [body.name, body.id];
+    console.log(sql);
+    conn.query(sql, params, function(err) {
+        if(err) console.log('query is not excuted. insert fail...\n' + err);
+        else res.redirect('/admin_login/admin_main');
+    });
+});
+
+app.post('/Pr_updataAf', function (req, res) {
+    var body = req.body;
+    console.log(body);
+
+    var sql = 'UPDATE `menu` SET price = ? WHERE id =?';
+    var params = [body.price, body.id];
+    console.log(sql);
+    conn.query(sql, params, function(err) {
+        if(err) console.log('query is not excuted. insert fail...\n' + err);
+        else res.redirect('/admin_login/admin_main');
+    });
+});
+
+adminModule.initMethod.initAdminStaticPath("/admin_login/admin_main");//로그인 완료후 리다이렉트될 url
 
 app.use("/api",adminModule.createApiRouter());
 
