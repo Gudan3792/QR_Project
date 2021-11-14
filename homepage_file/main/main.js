9fe9376859363d846936b8c0c34a1d0da521ec14
Gudan3792 <k_akao@kakao.com>
2021-11-15 00:07:15 +0900

    cart.html 모달화

--- homepage_file/main/main.js
+++ homepage_file/main/main.js
@@ -26,16 +26,26 @@ function iamport(n,p){
 
     window.onload = function() {
  
-    function onClick() {
-        document.querySelector('.modal_wrap').style.display ='block';
-        document.querySelector('.black_bg').style.display ='block';
-    }   
-    function offClick() {
-        document.querySelector('.modal_wrap').style.display ='none';
-        document.querySelector('.black_bg').style.display ='none';
-    }
- 
-    document.getElementById('modal_btn').addEventListener('click', onClick);
-    document.querySelector('.modal_close').addEventListener('click', offClick);
- 
-};
+        var modal = {
+            open : function(){
+              $('#modal-wrapper').show();
+            },
+            close : function(){
+              $('#modal-wrapper').hide();    
+            }
+          };
+          $(document).on('click', '#modal-overlay', function(){
+            window.history.back();  
+            document.getElementsByTagName('body')[0].style.overflow = 'scroll'; 
+          }).on("click", "button[name='Cmodal']", function(){
+            window.history.pushState({}, 'modal', '/modal');
+            modal.open();
+            document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+            
+          });
+          
+          window.onpopstate = history.onpushstate = function(e) {
+              if(window.location.href.split('/').pop().indexOf('modal')===-1){ // 마지막 segment가 cards라면 모달이 아닌 리스트인 상태이어야한다.
+                  modal.close(); // 현재의 모달을 닫는다.
+              }
+          }}
