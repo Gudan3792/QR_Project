f8b6cb8d46b24f88e413e39596c59bdc080ebf10
Gudan3792 <k_akao@kakao.com>
2021-11-14 20:59:11 +0900

    주문용 데이터베이스 만듬

--- 1.sql
+++ 1.sql
@@ -15,3 +15,12 @@ INSERT INTO `menu` VALUES (5,'버블티',7000,NOW());
 INSERT INTO `menu` VALUES (6,'ZICO',8000,NOW());
 INSERT INTO `menu` VALUES (7,'짬뽕',7000,NOW());
 INSERT INTO `menu` VALUES (8,'생맥주',3000,NOW());
+
+CREATE TABLE `sales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30),
+  `price` int(11),
+  `total_price` int(11),
+  `created` datetime,
+  PRIMARY KEY (`id`)
+);
