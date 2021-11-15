ce35b1229c752ee20598373f8616fd516e2f47d3
Gudan3792 <k_akao@kakao.com>
2021-11-16 00:07:55 +0900

    데이터베이스 테이블 이미지 주소를 위하여 수정

--- 1.sql
+++ 1.sql
@@ -3,24 +3,26 @@ CREATE TABLE `menu` (
   `name` varchar(30) NOT NULL,
   `price` int(11) NOT NULL,
   `created` datetime NOT NULL,
+  `img` varchar(300) NOT NULL,
   PRIMARY KEY (`id`)
 );
  
  
-INSERT INTO `menu` VALUES (1,'자장면',3000,NOW());
-INSERT INTO `menu` VALUES (2,'깐풍기',4000,NOW());
-INSERT INTO `menu` VALUES (3,'탕수육',5000,NOW());
-INSERT INTO `menu` VALUES (4,'족발',6000,NOW());
-INSERT INTO `menu` VALUES (5,'버블티',7000,NOW());
-INSERT INTO `menu` VALUES (6,'ZICO',8000,NOW());
-INSERT INTO `menu` VALUES (7,'짬뽕',7000,NOW());
-INSERT INTO `menu` VALUES (8,'생맥주',3000,NOW());
+INSERT INTO `menu` VALUES (1,'자장면',3000,NOW(),'https://via.placeholder.com/365');
+INSERT INTO `menu` VALUES (2,'깐풍기',4000,NOW(),'https://via.placeholder.com/365');
+INSERT INTO `menu` VALUES (3,'탕수육',5000,NOW(),'https://via.placeholder.com/365');
+INSERT INTO `menu` VALUES (4,'족발',6000,NOW(),'https://via.placeholder.com/365');
+INSERT INTO `menu` VALUES (5,'버블티',7000,NOW(),'https://via.placeholder.com/365');
+INSERT INTO `menu` VALUES (6,'ZICO',8000,NOW(),'https://via.placeholder.com/365');
+INSERT INTO `menu` VALUES (7,'짬뽕',7000,NOW(),'https://via.placeholder.com/365');
+INSERT INTO `menu` VALUES (8,'생맥주',3000,NOW(),'https://via.placeholder.com/365');
 
 CREATE TABLE `sales` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30),
-  `price` int(11),
   `total_price` int(11),
   `created` datetime,
   PRIMARY KEY (`id`)
 );
+
+INSERT INTO `sales` VALUES (1,'testData',3000,NOW());
