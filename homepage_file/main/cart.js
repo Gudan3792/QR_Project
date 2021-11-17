ea312dccb4cc648131b4d7528afc6fbc7d749c29
Gudan3792 <k_akao@kakao.com>
2021-11-18 01:22:04 +0900

    결제모듈 이니시스로 변경

--- homepage_file/main/cart.js
+++ homepage_file/main/cart.js
@@ -44,32 +44,31 @@ let basket = {
         this.updateUI();
     },
 
-     iamport: function(){
-        //가맹점 식별코드
-        IMP.init('imp72396912');
-        IMP.request_pay({
-            pg : 'kakaopay',
-            pay_method : 'card',
-            merchant_uid : 'merchant_' + new Date().getTime(),
-            name : this.totalCount+" 개의 상품" , //결제창에서 보여질 이름
-            amount : this.totalPrice, //실제 결제되는 가격
-        }, function(rsp) {
-            console.log(rsp);
-            if ( rsp.success ) {
-                var msg = '결제가 완료되었습니다.';
-                msg += '고유ID : ' + rsp.imp_uid;
-                msg += '상점 거래ID : ' + rsp.merchant_uid;
-                msg += '결제 금액 : ' + rsp.paid_amount;
-                msg += '카드 승인번호 : ' + rsp.apply_num;
-            } else {
-                 var msg = '결제에 실패하였습니다.';
-                 msg += '에러내용 : ' + rsp.error_msg;
-            }
-            // alert(msg);
-            window.location.href='views/main.ejs'
-            location.reload();
-        });
-    },
+    iamport: function(){
+        IMP.init('imp72396912');        //가맹점 식별코드
+            IMP.request_pay({
+                pg : 'inicis',
+                pay_method : 'card',
+                merchant_uid : 'merchant_' + new Date().getTime(),
+                name : this.totalCount+" 개의 상품" , //결제창에서 보여질 이름
+                amount : this.totalPrice, //실제 결제되는 가격
+            }, function(rsp) {
+                console.log(rsp);
+                if ( rsp.success ) {
+                    var msg = '결제가 완료되었습니다.';
+                    msg += '고유ID : ' + rsp.imp_uid;
+                    msg += '상점 거래ID : ' + rsp.merchant_uid;
+                    msg += '결제 금액 : ' + rsp.paid_amount;
+                    msg += '카드 승인번호 : ' + rsp.apply_num;
+                } else {
+                     var msg = '결제에 실패하였습니다.';
+                     msg += '에러내용 : ' + rsp.error_msg;
+                }
+                window.location.href='./main.ejs'
+                location.reload();
+    
+            });
+    }
 }
 
 // 숫자 3자리 콤마찍기
